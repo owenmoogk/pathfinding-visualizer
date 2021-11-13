c703ccaacc9bc96b7e6760988007213b88319c36
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2021-11-13 16:26:00 -0500

    added queue datastructure

---
+++ src/Queue.js
@@ -0,0 +1,15 @@
+export default function Queue() {
+    this.elements = []
+
+    Queue.prototype.add = function(e) {
+        this.elements.push(e)
+    }
+
+    Queue.prototype.dequeue = function () {
+        return this.elements.shift()
+    }
+
+    Queue.prototype.isEmpty = function () {
+        return this.elements.length == 0
+    }
+}
